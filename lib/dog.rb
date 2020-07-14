4f65023be241178f35ae27eb975d48117a5687e7
Dani Bassett <dani@danithetallgirl.com>
2020-07-14 02:27:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,12 @@
 class Dog
-  def initialize(name)
-    @name = name
+  def name=(dog_name)
+    this_dogs_name = dog_name
   end
  
   def name
-    @name
+    this_dogs_name
   end
 end
  
 fido = Dog.new("fido")
-
 fido.name
